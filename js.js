7d2b71765054b0788168b8bc928104a7b9de618d
E.SANTHOSH <santhoshcse18@gmail.com>
2023-04-26 23:17:03 -0700

    js

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 var images = [];
 
 images[0] = ['./img/ant.jpg'];
-images[1] = ['./img/avatar.jpg'];
+images[1] = ['./img/avatar.jfif'];
 images[2] = ['./img/jw4.jpg'];
-images[3] = ['./img/pathu.jpg'];
-images[4] = ['./img/viduthala.jpg'];
+images[3] = ['./img/Dr_Strang.jfif'];
+images[4] = ['./img/shazam.jfif'];
 var index = 0;
 
 function change() {
@@ -15,7 +15,57 @@ function change() {
     index++;
   }
 
-  setTimeout(change, 3000);
+  setTimeout(change, 2000);
 }
 
 window.onload = change();
+
+var images1 = [];
+
+images1[0] = ['./img/wednesday.webp'];
+images1[1] = ['./img/alice.jfif'];
+images1[2] = ['./img/loki.jfif'];
+images1[3] = ['./img/SHERLOCK.jfif'];
+images1[4] = ['./img/aoud.png'];
+var index1 = 0;
+
+function change1() {
+  document.getElementById("mainPhoto1").src = images1[index1];
+  if (index1 == 4) {
+    index1 = 0;
+  } else {
+    index1++;
+  }
+
+  setTimeout(change1, 2000);
+}
+window.onload = change1();
+
+var view=document.getElementById("a-pic");
+var button=document.getElementById("button");
+var text=document.getElementById("sm");
+button.onclick=function(){
+    if(view.className=="open"){
+        view.className="close";
+        text.innerHTML="Show More";
+    }
+    else{
+        view.className="open";
+        text.innerHTML="Show Less";
+    }
+};
+
+const tabs=document.querySelector('.tabs');
+const panels =document.querySelectorAll(".panel");
+tabs.addEventListener("click",function(e){
+    if(e.target.tagName=="BUTTON"){
+      const targetPanel=document.querySelector(e.target.dataset.target);
+      panels.forEach(function(panel){
+        if(panel==targetPanel){
+          panel.classList.add('active');
+        }else{
+          panel.classList.remove('active');
+        }
+      })
+    }
+});
